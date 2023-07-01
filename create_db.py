0f83697c07e8819b787346b592aa3615dbd3784d
MarcinZB <marcin.zbroszczyk@proton.me>
2023-07-01 12:06:49 +0200

    Adding a connection to an external instance (server) on which the database will be created

--- create_db.py
+++ create_db.py
@@ -1,5 +1,11 @@
+import os
+
 from psycopg2 import connect, OperationalError
 from psycopg2.errors import DuplicateDatabase, DuplicateTable
+from dotenv import load_dotenv
+
+load_dotenv()
+url = os.getenv("DATABASE_URL")
 
 CREATE_DATABASE = "CREATE DATABASE messanger_db"
 
@@ -16,9 +22,15 @@ CREATE_TABLE_MESSAGES = """CREATE TABLE messages
                         message_id serial ,
                         from_id int,
                         to_id int,
-                        creation_date timestamp,
+                        creation_date timestamp DEFAULT current_timestamp,
                         text varchar(255)
                         PRIMARY KEY (id),
-                        FOREIGN KEY (from_id) REFERENCES users(user_id),
-                        FOREIGN KEY (to_id) REFERENCES users(user_id)
+                        FOREIGN KEY (from_id) REFERENCES users(user_id) ON DELETE CASCADE ,
+                        FOREIGN KEY (to_id) REFERENCES users(user_id) ON DELETE CASCADE 
                     );"""
+
+try:
+    connection = connect(url)
+
+except OperationalError:
+    print("ERROR!")
