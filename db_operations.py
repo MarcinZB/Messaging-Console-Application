05d2a6910c7831e47cfb9a135dbe663dd18bc794
MarcinZB <marcin.zbroszczyk@proton.me>
2023-07-01 15:39:06 +0200

    Load user by id and username added

--- db_operations.py
+++ db_operations.py
@@ -1,4 +1,5 @@
 import crypto
+from psycopg2 import connect
 
 
 class User:
@@ -63,7 +64,7 @@ class User:
             """
         if self._id == -1:
             sql = """INSERT INTO users(username, hashed_password) 
-                     VALUES (%s, %s) RETURNING id"""
+                     VALUES (%s, %s) RETURNING user_id"""
             values = (self.username, self.hashed_password)
             cursor.execute(sql, values)
             self._id = cursor.fetchone()[0]
@@ -72,7 +73,69 @@ class User:
             sql = """UPDATE user SET username = %s, hashed_password=%s
                      WHERE id = %s"""
             values = (self.username, self.hashed_password, self.id)
-            cursor.execute(sql,values)
+            cursor.execute(sql, values)
             return True
 
+    @staticmethod
+    def load_user_by_username(cursor, username):
+        """Load a user from the database by username.
+
+            Args:
+                cursor: The cursor object used to execute the SQL query.
+                username (str): The username of the user to load.
+
+            Returns:
+                User or None: The loaded User object if the user is found, None otherwise.
+
+            Raises:
+                psycopg2.Error: If there is an error executing the SQL query.
+            """
+        sql = """SELECT user_id, username, hashed_password
+                 FROM users
+                 WHERE username=%s
+                    """
+
+        cursor.execute(sql, (username,))
+        data = cursor.fetchone()
+        if data:
+            id_, username, hashed_password = data
+            loaded_user = User(username)
+            loaded_user._id = id_
+            loaded_user._hashed_password = hashed_password
+            return loaded_user
+        else:
+            print("There is no user with this username")
+            return data
+
+    @staticmethod
+    def load_user_by_id(cursor, user_id):
+
+        sql = """SELECT user_id, username, hashed_password
+                 FROM users
+                 WHERE user_id=%s
+               """
+
+        cursor.execute(sql, (user_id,))
+        data = cursor.fetchone()
+
+        if data:
+            id_, username, hashed_password = data
+            loaded_user = User(username)
+            loaded_user._id = id_
+            loaded_user._hashed_password = hashed_password
+            return loaded_user
+        else:
+            print("There is no user with this id number ")
+
+
+    def load_all_users(self, cursor):
+        pass
+
+
+
+adam = User("Adam", "admin1")
+connection = connect(user="postgres", password="coderslab", host="localhost", database='messanger_db')
+connection.autocommit = True
+cursor = connection.cursor()
+print(adam.load_user_by_id(cursor, 1))
 
